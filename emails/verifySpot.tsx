41e6eb9a2e6c0466738f158d96e987a609c7189f
Basti Buck <mail@bastibuck.de>
2024-10-06 17:26:16 +0200

    send notification email on subscription

---
+++ emails/verifySpot.tsx
@@ -0,0 +1,132 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
+interface VerifyEmailProps {
+  spotName: string;
+  securityToken: string;
+}
+
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "";
+
+const VerifySpotSubscriptionEmail = ({
+  spotName,
+  securityToken,
+}: VerifyEmailProps) => (
+  <Html>
+    <Head />
+    <Preview>
+      Verify your subscription to
+      {spotName}
+    </Preview>
+
+    <Body style={main}>
+      <Container style={container}>
+        <Heading style={heading}>
+          <Text style={{ ...heading, display: "inline" }}>
+            Verify your subscription to{" "}
+          </Text>
+          <Text style={{ ...heading, display: "inline", fontWeight: "bold" }}>
+            {spotName}
+          </Text>
+        </Heading>
+
+        <Section style={buttonContainer}>
+          <Button style={button} href={`${baseUrl}/verify/${securityToken}`}>
+            Verify subscription
+          </Button>
+        </Section>
+
+        <Text style={paragraph}>
+          Once you verify your subscription to {spotName}, you will be notified
+          about suitable wind conditions for your spot.
+        </Text>
+
+        <Text style={paragraph}>
+          You can unsubscribe at any time by clicking the link at the bottom of
+          a notification email.
+        </Text>
+
+        <Hr style={hr} />
+
+        <Link href={`${baseUrl}`} style={spotterLink}>
+          Spotter
+        </Link>
+      </Container>
+    </Body>
+  </Html>
+);
+
+export default VerifySpotSubscriptionEmail;
+
+VerifySpotSubscriptionEmail.PreviewProps = {
+  securityToken: "tt226-5398x",
+  spotName: "Aukrog",
+};
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 0 48px",
+  maxWidth: "560px",
+};
+
+const heading = {
+  fontSize: "24px",
+  letterSpacing: "-0.5px",
+  lineHeight: "1.3",
+  fontWeight: "400",
+  color: "#484848",
+  padding: "17px 0 0",
+};
+
+const paragraph = {
+  margin: "0 0 10px",
+  fontSize: "15px",
+  lineHeight: "1.4",
+  color: "#3c4149",
+};
+
+const buttonContainer = {
+  padding: "10px 0 20px",
+};
+
+const button = {
+  backgroundColor: "#35b8e0",
+  borderRadius: "3px",
+  fontWeight: "600",
+  color: "#fff",
+  fontSize: "15px",
+  textDecoration: "none",
+  textAlign: "center" as const,
+  display: "block",
+  padding: "11px 23px",
+};
+
+const spotterLink = {
+  fontSize: "14px",
+  color: "#35b8e0",
+};
+
+const hr = {
+  borderColor: "#dfe1e4",
+  margin: "26px 0 26px",
+};
--- src/server/api/routers/subscription.ts
+++ src/server/api/routers/subscription.ts
@@ -1,18 +1,77 @@
-import { eq } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
+import { and, eq } from "drizzle-orm";
+import VerifySpotSubscriptionEmail from "emails/verifySpot";
+import { Resend } from "resend";
 import { z } from "zod";
+import { env } from "~/env";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { subscriptions } from "~/server/db/schema";
+import { spots, subscriptions } from "~/server/db/schema";
+
+const resend = new Resend(env.RESEND_API_KEY);
 
 export const subscriptionRouter = createTRPCRouter({
   subscribe: publicProcedure
     .input(z.object({ email: z.string().email(), spotId: z.number().int() }))
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.insert(subscriptions).values({
-        email: input.email,
-        spotId: input.spotId,
+      const spot = await ctx.db.query.spots.findFirst({
+        where: eq(spots.id, input.spotId),
       });
 
-      // TODO! send verification email with link to verification page
+      if (spot === undefined) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Spot not found.",
+        });
+      }
+
+      const subscription = await ctx.db
+        .insert(subscriptions)
+        .values({
+          email: input.email,
+          spotId: spot.id,
+        })
+        .returning({ securityToken: subscriptions.securityToken });
+
+      const securityToken = subscription.at(0)?.securityToken ?? null;
+
+      if (securityToken === null) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to create subscription token.",
+        });
+      }
+
+      try {
+        const { error } = await resend.emails.send({
+          from: env.FROM_EMAIL,
+          to: input.email,
+          subject: `Verify your subscription to ${spot.name}`,
+          react: VerifySpotSubscriptionEmail({
+            spotName: spot.name,
+            securityToken,
+          }),
+        });
+
+        // TODO? add logging for errors
+
+        if (error !== null) {
+          throw new Error("error");
+        }
+      } catch {
+        await ctx.db
+          .delete(subscriptions)
+          .where(
+            and(
+              eq(subscriptions.email, input.email),
+              eq(subscriptions.spotId, spot.id),
+            ),
+          );
+
+        throw new TRPCError({
+          code: "SERVICE_UNAVAILABLE",
+          message: "The email service is currently unavailable.",
+        });
+      }
     }),
 
   verify: publicProcedure
